ee44eb772cb5705729a91707bdbd09367c35a14a
Jason Liu <jasonliu00@gmail.com>
2025-03-20 21:25:27 -0400

    Refactor and write more efficient assembly code

--- VMTranslator.py
+++ VMTranslator.py
@@ -31,6 +31,8 @@ class Translate:
         lines = contents.splitlines()
 
         with open(f'{self.output_file}', "w") as file_stream:
+            self.file_stream = file_stream
+
             for line in lines:
                 stripped_line = line.partition("//")[0].strip()
                 
@@ -38,7 +40,7 @@ class Translate:
                     continue
 
                 parsed_components = self.parse(stripped_line)
-                self.write_assembly(parsed_components, file_stream)
+                self.write_to_asm(parsed_components)
     
 
     def parse(self, line: str) -> List:
@@ -62,189 +64,205 @@ class Translate:
         
         return components
     
+    def write(self, code: str) -> None:
+        print(code, file=self.file_stream)
+    
 
-    def pop(self, file: TextIO, register: str) -> None:
-        print("@SP", file=file)
-        print("M=M-1", file=file)
-        print("@SP", file=file)
-        print("A=M", file=file)
-        print("D=M", file=file)
-        print(register, file=file)
-        print("M=D", file=file)
+    def pop(self) -> None:
+        # decrement stack pointer first
+        self.write("@SP")
+        self.write("M=M-1")
+
+        self.write("@SP")
+        self.write("A=M")
         
     
-    def pop_and_store_in_register(self, file: TextIO, register=None, index=None) -> None:
-        temp_popped_val = self.R1
-        self.pop(file, register=temp_popped_val)
+    def pop_and_store(self, register=None, index=None) -> None:
 
         if register == "pointer":
             pointer = "THIS" if index == 0 else "THAT"
-            print(temp_popped_val, file=file)
-            print("D=M", file=file)
-            print(f"@{pointer}", file=file)
-            print("M=D", file=file)
+            self.pop()
+            self.write("D=M")
+            self.write(f"@{pointer}")
+            self.write("M=D")
             return
 
         elif register == "constant":
-            print(f"@{index}", file=file)
-            print("D=A", file=file)
-        elif register == "static":
-            print(f"@{self.ref}.{index}", file=file)
-            print("D=A", file=file)
-        elif register == "temp":
-            print(f"@{index}", file=file)
-            print("D=A", file=file)
-            print("@5", file=file)
-            print("D=D+A", file=file)
-        else:
-            print(f"@{index}", file=file)
-            print("D=A", file=file)
-            if register == "local":
-                print("@LCL", file=file)
-            elif register == "argument":
-                print("@ARG", file=file)
-            elif register == "this":
-                print("@THIS", file=file)
-            elif register == "that":
-                print("@THAT", file=file)
+            self.pop()
+            self.write("D=M")
+            self.write(f"@{index}")
+            self.write("M=D")
 
-            print("D=D+M", file=file)
-        
-        print(self.R2, file=file)
-        print("M=D", file=file) # temporarily store target index in Register 2
-        print(temp_popped_val, file=file)
-        print("D=M", file=file) # now transfer popped value to D
-        print(self.R2, file=file)
-        print("A=M", file=file)
-        print("M=D", file=file) # set register[index] to popped value
+        elif register == "static":
+            self.pop()
+            self.write("D=M")
+            self.write(f"@{self.ref}.{index}")
+            self.write("M=D")
+
+        elif register == "temp":
+            # store index in R13
+            self.write(f"@{index}")
+            self.write("D=A")
+            self.write("@5")
+            self.write("D=D+A")
+            self.write(self.R1)
+            self.write("M=D")
+            
+            # pop
+            self.pop()
+            self.write("D=M")
+
+            # store popped value into index that was stored in R13
+            self.write(self.R1)
+            self.write("A=M")
+            self.write("M=D")
+
+        else:
+            self.write(f"@{index}")
+            self.write("D=A")
+            if register == "local":
+                self.write("@LCL")
+            elif register == "argument":
+                self.write("@ARG")
+            elif register == "this":
+                self.write("@THIS")
+            elif register == "that":
+                self.write("@THAT")
+
+            # store index in R13
+            self.write("D=D+M")
+            self.write(self.R1)
+            self.write("M=D")
+            
+            # pop
+            self.pop()
+            self.write("D=M")
+
+            # store popped value into index that was stored in Register 1
+            self.write(self.R1)
+            self.write("A=M")
+            self.write("M=D")
 
     
-    def get_val_from_register_and_push(self, file: TextIO, register=None, index=None) -> None:
+    def get_and_push(self, register=None, index=None) -> None:
+
         if register == "pointer":
             pointer = "THIS" if index == 0 else "THAT"
-            print(f"@{pointer}", file=file)
-            print("D=M", file=file)
+            self.write(f"@{pointer}")
+            self.write("D=M")
+
         elif register == "static":
-            print(f"@{self.ref}.{index}", file=file)
-            print("D=M", file=file)
+            self.write(f"@{self.ref}.{index}")
+            self.write("D=M")
+
         elif register == "constant":
-            print(f"@{index}", file=file)
-            print("D=A", file=file)
+            self.write(f"@{index}")
+            self.write("D=A")
+
         elif register == "temp":
-            print(f"@{index}", file=file)
-            print("D=A", file=file)
-            print("@5", file=file)
-            print("A=D+A", file=file)
-            print("D=M", file=file)
+            self.write(f"@{index}")
+            self.write("D=A")
+            self.write("@5")
+            self.write("A=D+A")
+            self.write("D=M")
+
         else:
-            print(f"@{index}", file=file)
-            print("D=A", file=file)
+            self.write(f"@{index}")
+            self.write("D=A")
             if register == "local":
-                print("@LCL", file=file)
+                self.write("@LCL")
             elif register == "argument":
-                print("@ARG", file=file)
+                self.write("@ARG")
             elif register == "this":
-                print("@THIS", file=file)
+                self.write("@THIS")
             elif register == "that":
-                print("@THAT", file=file)
+                self.write("@THAT")
 
-            print("A=D+M", file=file)
-            print("D=M", file=file)
+            self.write("A=D+M")
+            self.write("D=M")
 
-        temp_val_to_push = self.R1
-        print(temp_val_to_push, file=file)
-        print("M=D", file=file) # store value from register[index] to a temp register
-
-        self.push(file, temp_val_to_push)
+        self.push()
     
 
-    def push(self, file: TextIO, register: str) -> None:
-        # first store the val we want to push to D
-        print(register, file=file)
-        print("D=M", file=file)
-
-        # get stack pointer
-        print("@SP", file=file)
-        print("A=M", file=file)
-
-        # store D in memory of stack pointer
-        print("M=D", file=file)
+    def push(self) -> None:
+        self.write("@SP")
+        self.write("A=M")
+        self.write("M=D")
 
         # increment stack pointer
-        print("@SP", file=file)
-        print("M=M+1", file=file)
+        self.write("@SP")
+        self.write("M=M+1")
     
-    def perform_arithmetic(self, cmd: str, file: TextIO) -> None:
-        # pop element and store in Register 1
-        self.pop(file, register=self.R1)
+    def perform_arithmetic(self, cmd: str) -> None:
+        self.pop()
+        self.write("D=M")
 
         if cmd in ["neg", "not"]:
-            print(self.R1, file=file)
             if cmd == "neg":
-                print("D=-M", file=file)
+                self.write("D=-D")
             else:
-                print("D=!M", file=file)
-            print(self.R3, file=file)
-            print("M=D", file=file)
-            self.push(file, register=self.R3)
+                self.write("D=!D")
+
+            # get stack pointer
+            self.write("@SP")
+            self.write("A=M")
+
+            # store D in memory of stack pointer
+            self.write("M=D")
+
+            # increment stack pointer
+            self.write("@SP")
+            self.write("M=M+1")
             return
 
-        # pop next element and store in Register 2
-        self.pop(file, register=self.R2)
-        
-        print(self.R1, file=file)
-        print("D=M", file=file)
-        print(self.R2, file=file)
+        # pop second value
+        self.pop()
 
         # perform op and store result in D
         if cmd == "add":
-            print("D=D+M", file=file)
+            self.write("D=D+M")
         elif cmd == "sub":
-            print("D=M-D", file=file)
+            self.write("D=M-D")
         elif cmd == "and":
-            print("D=D&M", file=file)
+            self.write("D=D&M")
         elif cmd == "or":
-            print("D=D|M", file=file)
+            self.write("D=D|M")
         else:
             if cmd == "eq":
-                print("D=M-D", file=file)
-                print(f"@SET_TRUE{self.op_count}", file=file)
-                print("D;JEQ", file=file)
+                self.write("D=M-D")
+                self.write(f"@SET_TRUE{self.op_count}")
+                self.write("D;JEQ")
             elif cmd == "gt":
-                print("D=M-D", file=file)
-                print(f"@SET_TRUE{self.op_count}", file=file)
-                print("D;JGT", file=file)
+                self.write("D=M-D")
+                self.write(f"@SET_TRUE{self.op_count}")
+                self.write("D;JGT")
             elif cmd == "lt":
-                print("D=M-D", file=file)
-                print(f"@SET_TRUE{self.op_count}", file=file)
-                print("D;JLT", file=file)
+                self.write("D=M-D")
+                self.write(f"@SET_TRUE{self.op_count}")
+                self.write("D;JLT")
 
-            print("D=0", file=file)
-            print(f"@END{self.op_count}", file=file)
-            print("0;JMP", file=file)
+            self.write("D=0") # set D to false
+            self.write(f"@END{self.op_count}")
+            self.write("0;JMP")
 
-            print(f"(SET_TRUE{self.op_count})", file=file)
-            print("D=-1", file=file) # set D equal to true (-1, aka 1111111111111111)
+            self.write(f"(SET_TRUE{self.op_count})")
+            self.write("D=-1") # set D to true (-1, aka 1111111111111111)
 
-            print(f"(END{self.op_count})", file=file)
+            self.write(f"(END{self.op_count})")
             self.op_count += 1
 
-        # store result from D into Register 3
-        print(self.R3, file=file)
-        print("M=D", file=file)
-
-        self.push(file, register=self.R3)
+        self.push()
 
     
-    def write_assembly(self, components: List[str], file: TextIO) -> None:
+    def write_to_asm(self, components: List[str]) -> None:
         cmd = components[0]
 
         if len(components) == 1:
-            self.perform_arithmetic(cmd, file)
+            self.perform_arithmetic(cmd)
         elif cmd == "push":
-            self.get_val_from_register_and_push(file=file, register=components[1], index=components[2])
+            self.get_and_push(register=components[1], index=components[2])
         elif cmd == "pop":
-            self.pop_and_store_in_register(file=file, register=components[1], index=components[2])
+            self.pop_and_store(register=components[1], index=components[2])
         else:
             print(f"Error: the command '{segment}' has not been implemented yet.")
             sys.exit(1)
